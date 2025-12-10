a11db56539a6bad23e31c84a4bbc5bf3ecb420e5
asetbsb <aset.bsb@gmail.com>
2025-12-10 19:36:09 +0500

    Easy, medium and starter of hard task

---
+++ student-attendance-boot/src/test/java/org/example/student_attendance_boot/StudentAttendanceBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.student_attendance_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAttendanceBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
